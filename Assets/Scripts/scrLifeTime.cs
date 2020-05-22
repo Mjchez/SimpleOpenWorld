bb3b8a06f586e25658789bf9e0af9569c4b15852
Mjchez <51550417+Mjchez@users.noreply.github.com>
2020-05-21 22:17:09 -0300

    Criacao completa - Armas

---
+++ Assets/Scripts/scrLifeTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrLifeTime : MonoBehaviour
+{
+    public float time=3;
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
