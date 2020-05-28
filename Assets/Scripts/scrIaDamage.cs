6ef2179e2abb987595e9006baf3d2eedc68e4278
Mjchez <51550417+Mjchez@users.noreply.github.com>
2020-05-28 19:45:52 -0300

    Prototipo IA Bugado

---
+++ Assets/Scripts/scrIaDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrIaDamage : MonoBehaviour
+{
+    public int lives =10;
+    public scrIaStar iaStar;
+    
+    
+    void Update()
+    {
+      if(lives < 0)
+      {
+          iaStar.Dead();
+          Destroy(gameObject,4);
+      }  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("PlayerProjectile"))
+        {
+            lives--;
+            iaStar.Damage();
+        }
+    }
+
+    public void ExplosionDamage()
+    {
+        lives =-1;
+    }
+
+
+
+}
